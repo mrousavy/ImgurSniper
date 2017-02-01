84072e44277c5a6993d0ba958c2cc2a1f875d8fa
dmaniak <dmaniak@student.tgm.ac.at>
2017-02-01 21:38:33 +0100

    Fixed English & German Resource Implementation

--- AddToContextMenu/Program.cs
+++ AddToContextMenu/Program.cs
@@ -7,7 +7,7 @@ namespace AddToContextMenu {
     class Program {
         static void Main(string[] args) {
             string lang = Thread.CurrentThread.CurrentCulture.EnglishName;
-
+            
             try {
                 string dir = AppDomain.CurrentDomain.BaseDirectory;
                 string path = Path.Combine(dir, "ImgurSniper.exe");
@@ -22,12 +22,12 @@ namespace AddToContextMenu {
                     }
                 }
 
-                if(lang == "German")
+                if(lang.Contains("German"))
                     Console.Write(Properties.german.addContextMenu);
                 else
                     Console.Write(Properties.english.addContextMenu);
             } catch {
-                if(lang == "German")
+                if(lang.Contains("German"))
                     Console.Write(Properties.german.errorContext);
                 else
                     Console.Write(Properties.english.errorContext);
