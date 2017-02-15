acd165e44ccd6e3b25cd21894cdc852e1bfa7892
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-02-15 10:48:58 +0100

    Fixed potential white spots in Screencapture
    Removed the Alpha Channel from PixelFormat for no White spots

--- ImgurSniper/Screenshot.cs
+++ ImgurSniper/Screenshot.cs
@@ -16,7 +16,7 @@ namespace ImgurSniper {
             int height = coordinates.Height;
 
             //Thanks http://stackoverflow.com/users/183367/julien-lebosquain!        --System.Drawing.Imaging.PixelFormat.Format32bppArgb
-            using(var screenBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format64bppPArgb)) {
+            using(var screenBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppRgb)) {
                 using(var bmpGraphics = Graphics.FromImage(screenBmp)) {
                     bmpGraphics.CopyFromScreen(start, System.Drawing.Point.Empty, new System.Drawing.Size(width, height));
 
