dee015547507c0273686eb7c45c2a2a2e5e04423
mrousavy <marcrousavy@hotmail.com>
2017-03-02 13:51:09 +0100

    ImgurSniper Launch from Installer

--- ImgurSniper.UI/App.xaml.cs
+++ ImgurSniper.UI/App.xaml.cs
@@ -1,19 +1,20 @@
 ﻿using ImgurSniper.UI.Properties;
+using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Markup;
-using System.Windows.Threading;
 
 namespace ImgurSniper.UI {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
+    public partial class App {
 
-        public App() : base() {
-            DispatcherUnhandledException += (object sender, DispatcherUnhandledExceptionEventArgs e) => {
+        public App() {
+            DispatcherUnhandledException += (s, e) => {
                 if(MessageBox.Show($"{strings.unhandledError}({e.Exception.Message})",
                     "ImgurSniper Error",
                     MessageBoxButton.YesNo,
@@ -29,11 +30,29 @@ namespace ImgurSniper.UI {
                 Process.GetCurrentProcess().Kill();
             };
 
+            IsInstaller();
+
             string language = FileIO.Language;
             Thread.CurrentThread.CurrentCulture = new CultureInfo(language);
             Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
             FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
                         XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
+
+
+
+
+        private void IsInstaller() {
+            //Restard if Argument "Installer" is passed (From CustomActions)
+            //(Because Installer will wait for Process Exit)
+            string[] args = Environment.GetCommandLineArgs();
+            if(args.Length > 0 && args.Contains("Installer")) {
+                string fileName = System.Reflection.Assembly.GetEntryAssembly().Location;
+                if(fileName != null)
+                    Process.Start(fileName);
+
+                System.Windows.Application.Current.Shutdown(0);
+            }
         }
     }
 }
