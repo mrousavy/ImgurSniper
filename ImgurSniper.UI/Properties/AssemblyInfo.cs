4c715faf641e19a7f36840aa9ec566e497a0d704
mrousavy <marcrousavy@hotmail.com>
2017-03-30 14:50:31 +0200

    R: Rebuilt Installer

--- ImgurSniper.UI/Properties/AssemblyInfo.cs
+++ ImgurSniper.UI/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.5.6")]
-[assembly: AssemblyFileVersion("1.0.5.6")]
+[assembly: AssemblyVersion("1.0.6.4")]
+[assembly: AssemblyFileVersion("1.0.6.4")]
